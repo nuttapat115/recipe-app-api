826744e11fee5256b4ee79a16d83dee6fbe4afc6
nuttapat115 <nuttapat115_@hotmail.com>
2023-05-31 00:30:57 +0700

    added api for upload image to recipe

--- app/app/settings.py
+++ app/app/settings.py
@@ -144,3 +144,7 @@ AUTH_USER_MODEL = 'core.User'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+SPECTACULAR_SETTING = {
+    'COMPONENT_SPLIT_REQUEST': True,
+}
